a93f6f3dce9e687450c0cdaf7dd0749dd219df7d
lins <linsmariya124@gmail.com>
2022-08-29 16:20:58 +0530

    feat:Add Tagline image

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+const { url } = require('inspector');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        backgroundImage: {
+          'bw-pattern':"url('../src/img/bw.png')",
+       
+        },
+        backgroundSize: {
+            '40%':'40%',
+        },
+        fontFamily: {
+          'helvetica':'helvetica',
+        },
+      }
+  
+  },
+  plugins: [],
+}
