3dc101d53d0718f19241dc66114b24539a58eae0
lins <linsmariya124@gmail.com>
2022-09-05 12:20:39 +0530

    perf : Modify navbar

--- src/script.js
+++ src/script.js
@@ -1,11 +1,56 @@
-const { brotliDecompressSync } = require("zlib");
-
-function onClickDrop(){
-    var div=document.getElementsByClassName("myDropdown");
-    // div.style.display = div.style.display == "none" ? "block" : "none";
-    div.classList.toggle("show");
-    document.body.style.overflow="hidden";
+function onClickDrop1(){ 
+    // navdiv=document.getElementById("dropdown2").classList.toggle("show");
+    const navdiv1=document.getElementById("dropdown1");
+    const navdiv2=document.getElementById("dropdown2");
+    const navdiv3=document.getElementById("dropdown3");
+    if(navdiv1.style.display==="none"){
+        navdiv1.style.display="block";
+        navdiv2.style.display="none";
+        navdiv3.style.display="none";
+        document.body.style.overflow="hidden";
+    }
+    else {
+        navdiv1.style.display="none";
+        document.body.style.overflow="scroll";
+    }
+    
+    
+   
 }
-function onScrollSTop(){
-    document.body.style.overflow="hidden";
+   
+function onClickDrop2(){ 
+    // navdiv=document.getElementById("dropdown2").classList.toggle("show");
+    const navdiv1=document.getElementById("dropdown1");
+    const navdiv2=document.getElementById("dropdown2");
+    const navdiv3=document.getElementById("dropdown3");
+    if(navdiv2.style.display==="none"){
+        navdiv2.style.display="block";
+        navdiv1.style.display="none";
+        navdiv3.style.display="none";
+        document.body.style.overflow="hidden";
+    }
+    else {
+        navdiv2.style.display="none";
+        document.body.style.overflow="scroll";
+    }
+   
+   
+}
+function onClickDrop3(){ 
+    // navdiv=document.getElementById("dropdown1").classList.toggle("show");
+    const navdiv1=document.getElementById("dropdown1");
+    const navdiv2=document.getElementById("dropdown2");
+    const navdiv3=document.getElementById("dropdown3");
+    if(navdiv3.style.display==="none"){
+        navdiv3.style.display="block";
+        navdiv1.style.display="none";
+        navdiv2.style.display="none";
+        document.body.style.overflow="hidden";
+    }
+    else {
+        navdiv3.style.display="none";
+        document.body.style.overflow="scroll";
+    }
+  
+   
 }
